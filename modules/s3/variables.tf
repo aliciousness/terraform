9cc6dfc5d2748258efd559d201db39812278ac11
Richard Craddock <richard.craddock@alpinecyber.com>
2023-06-24 22:39:00 -0400

    finished state config and started building family bucket

---
+++ modules/s3/variables.tf
@@ -0,0 +1,6 @@
+variable "application" {}
+variable "environment" {}
+variable "key_id"{}
+variable "terraform_tags" {
+  description = "A simple array of tags used to indicate who, how and why a resource was made"
+}
